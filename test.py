4da5ad43315eb860fd1f2dd80e7e42a9249af72d
Young Petya <peter.pavicic@gmail.com>
2024-10-31 23:09:39 +0100

    added directory creating code to test.py

--- test.py
+++ test.py
@@ -163,3 +163,9 @@ hour = now.hour
 minute = now.minute
 print(f"{month}{day}/{hour:02}/{minute:02}")
 
+for month in ["oct", "nov"]:
+    for day in range(1,5):
+        for hour in range(24):
+            for minute in range(60):
+                pathname = f"{month}{day}/{hour:02}/{minute:02}"
+                os.makedirs(pathname)
