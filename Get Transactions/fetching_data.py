0a6eb4f3915cd185a4771f1723baabe2bb9787cd
Peter Pavicic <peter.pavicic@gmail.com>
2025-06-12 06:04:55 +0200

    Modified .gitignore to ignore pycache files, updated fetching_data to include argument for startPage (e.g. in case of code breaking down

--- Get Transactions/fetching_data.py
+++ Get Transactions/fetching_data.py
@@ -2,15 +2,18 @@
 
 from queries import *
 
-def getTrumpMarket():
+def getTrumpMarket(startPages: int | list[int]):
     trumpYes = '"21742633143463906290569050155826241533067272736897614950488156847949938836455"'
     trumpNo = '"48331043336612883890938759509493159234755048973500640148014422747788308965732"'
 
+    if isinstance(startPages, int):
+        startPages = [startPages]
+
     orderTimestamp = "\torderBy: timestamp\n\torderDirection: asc"
-    trumpElectionTransactionsMaker  = Subquery(SQ.OrdersFilled, name="Maker", orderText=orderTimestamp, filterText=f"makerAssetId_in: [{trumpYes},\n\t\t{trumpNo}]")
-    trumpElectionTransactionsTaker  = Subquery(SQ.OrdersFilled, name="Taker", orderText=orderTimestamp, filterText=f"takerAssetId_in: [{trumpYes},\n\t\t{trumpNo}]")
-    trumpElectionOrdersMaker        = Subquery(SQ.OrdersMatched, name="Maker", orderText=orderTimestamp, filterText=f"makerAssetID_in: [{trumpYes},\n\t\t{trumpNo}]")
-    trumpElectionOrdersTaker        = Subquery(SQ.OrdersMatched, name="Taker", orderText=orderTimestamp, filterText=f"takerAssetID_in: [{trumpYes},\n\t\t{trumpNo}]")
+    trumpElectionTransactionsMaker  = Subquery(SQ.OrdersFilled, name="Maker", orderText=orderTimestamp, filterText=f"makerAssetId_in: [{trumpYes},\n\t\t{trumpNo}]", startPage=startPages[0])
+    trumpElectionTransactionsTaker  = Subquery(SQ.OrdersFilled, name="Taker", orderText=orderTimestamp, filterText=f"takerAssetId_in: [{trumpYes},\n\t\t{trumpNo}]", startPage=startPages[1])
+    trumpElectionOrdersMaker        = Subquery(SQ.OrdersMatched, name="Maker", orderText=orderTimestamp, filterText=f"makerAssetID_in: [{trumpYes},\n\t\t{trumpNo}]", startPage=startPages[2])
+    trumpElectionOrdersTaker        = Subquery(SQ.OrdersMatched, name="Taker", orderText=orderTimestamp, filterText=f"takerAssetID_in: [{trumpYes},\n\t\t{trumpNo}]", startPage=startPages[3])
 
     subqueries = [trumpElectionTransactionsMaker,
                   trumpElectionTransactionsTaker,
@@ -25,7 +28,7 @@ def getTrumpMarket():
 
 
 if __name__ == "__main__":
-    getTrumpMarket()
+    getTrumpMarket([113000, 113000, 113000, 113000])
 
 
 
