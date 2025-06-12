baf074513c637a7048dbf6192b4e397aea4c7b7e
Peter Pavicic <peter.pavicic@gmail.com>
2025-06-12 04:50:08 +0200

    Rewrote getting Trump market data to function, which is how fetching_data.py should be structured from now on

--- Get Transactions/fetching_data.py
+++ Get Transactions/fetching_data.py
@@ -2,14 +2,11 @@
 
 from queries import *
 
-
-if __name__ == "__main__":
+def getTrumpMarket():
     trumpYes = '"21742633143463906290569050155826241533067272736897614950488156847949938836455"'
     trumpNo = '"48331043336612883890938759509493159234755048973500640148014422747788308965732"'
 
     orderTimestamp = "\torderBy: timestamp\n\torderDirection: asc"
-
-
     trumpElectionTransactionsMaker  = Subquery(SQ.OrdersFilled, name="Maker", orderText=orderTimestamp, filterText=f"makerAssetId_in: [{trumpYes},\n\t\t{trumpNo}]")
     trumpElectionTransactionsTaker  = Subquery(SQ.OrdersFilled, name="Taker", orderText=orderTimestamp, filterText=f"takerAssetId_in: [{trumpYes},\n\t\t{trumpNo}]")
     trumpElectionOrdersMaker        = Subquery(SQ.OrdersMatched, name="Maker", orderText=orderTimestamp, filterText=f"makerAssetID_in: [{trumpYes},\n\t\t{trumpNo}]")
@@ -21,6 +18,17 @@ if __name__ == "__main__":
                   trumpElectionOrdersTaker
                   ]
 
-    myQuery = Query("TrumpElection", SG.ORDERS_SG, subqueries)
+    myQuery = Query("TrumpElectionWinner", SG.ORDERS_SG, subqueries)
     print(myQuery.QueryText)
     myQuery.run_query(True)
+    print("Done getting Trump Market data")
+
+
+if __name__ == "__main__":
+    getTrumpMarket()
+
+
+
+
+
+
